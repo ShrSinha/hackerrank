6d517a223e4e0f244020cc74408c4c63dcd1ccf5
ShrSinha <shruti.sinha@gmail.com>
2017-09-24 10:14:58 -0700

    feat: Solution.

---
+++ sql/advanced-select/new_companies.sql
@@ -0,0 +1,16 @@
+/*
+Given the table schemas below, write a query to print the company_code, founder name, total number of lead managers, total number of senior managers, total number of managers, and total number of employees. Order your output by ascending company_code.
+*/
+select
+e.company_code
+,c.founder
+,count(distinct e.lead_manager_code)
+,count(distinct e.senior_manager_code)
+,count(distinct e.manager_code)
+,count(distinct e.employee_code)
+from 
+Employee e
+join Company c
+on e.company_code = c.company_code
+group by e.company_code,c.founder
+order by e.company_code asc;
