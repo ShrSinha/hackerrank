4a9da5b840f2dcc3388d88a8ddd5a593de7c8fa8
ShrSinha <shruti.sinha@gmail.com>
2017-10-02 09:10:37 -0700

    feat: Solution.

---
+++ algorithms/diagonal_difference.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import sys
+
+
+n = int(input().strip())
+a = []
+for a_i in range(n):
+    a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
+    a.append(a_t)
+
+total_diagonal_p = 0
+total_diagonal_s = 0
+for k in range(n):
+    total_diagonal_p += a[k][k]
+    total_diagonal_s += a[k][-1-k]
+print(abs(total_diagonal_p - total_diagonal_s))    
